cbe623bdfeb7af584999e4aa6dc84b0183d4aed4
[Weysinaction] <[lipatoff.const@gmail.com]>
2021-07-12 00:04:25 +0300

    init

---
+++ AppStore/AppStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStore
+//
+//  Created by Владислав Лазарев on 11.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
