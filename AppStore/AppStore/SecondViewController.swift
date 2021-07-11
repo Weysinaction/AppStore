3831695fdb7598b973715c4f3d1d7f68030cccf6
[Weysinaction] <[lipatoff.const@gmail.com]>
2021-07-12 00:11:46 +0300

    создал классы и добавил таб бар айтемы

--- AppStore/AppStore/SecondViewController.swift
+++ AppStore/AppStore/SecondViewController.swift
@@ -1,13 +1,13 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  AppStore
 //
-//  Created by Владислав Лазарев on 11.07.2021.
+//  Created by Владислав Лазарев on 12.07.2021.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,11 +17,10 @@ class ViewController: UIViewController {
 
     func setupTabBarItem() {
         var tabBarItem = UITabBarItem()
-        tabBarItem = UITabBarItem(title: "Купить", image: UIImage(systemName: "laptopcomputer.and.iphone"), tag: 0)
+        tabBarItem = UITabBarItem(title: "Для вас", image: UIImage(systemName: "person.crop.circle"), tag: 1)
         tabBarItem.badgeColor = .lightGray
         
         self.tabBarItem = tabBarItem
         self.view.backgroundColor = .black
     }
 }
-
