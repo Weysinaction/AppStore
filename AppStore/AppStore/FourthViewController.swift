138e8d568f61e572453146f1d93dbf170a5f74ba
[Weysinaction] <[lipatoff.const@gmail.com]>
2021-07-12 00:16:22 +0300

    Revert "Revert "создал классы и добавил таб бар айтемы""
    This reverts commit 2425e1d2cec52b6e2126c69087cf98ea4a697de4.

--- AppStore/AppStore/FourthViewController.swift
+++ AppStore/AppStore/FourthViewController.swift
@@ -1,27 +1,26 @@
 //
-//  ViewController.swift
+//  FourthViewController.swift
 //  AppStore
 //
-//  Created by Владислав Лазарев on 11.07.2021.
+//  Created by Владислав Лазарев on 12.07.2021.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class FourthViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupTabBarItem()
     }
-
+    
     func setupTabBarItem() {
         var tabBarItem = UITabBarItem()
-        tabBarItem = UITabBarItem(title: "Купить", image: UIImage(systemName: "laptopcomputer.and.iphone"), tag: 0)
+        tabBarItem = UITabBarItem(title: "Корзина", image: UIImage(systemName: "bag"), tag: 3)
         tabBarItem.badgeColor = .lightGray
         
         self.tabBarItem = tabBarItem
         self.view.backgroundColor = .black
     }
 }
-
